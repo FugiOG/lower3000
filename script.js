09f85378fdada87c6f740579ad6259cc18a01d25
Fugi <rememberov.andrei@yandex.ru>
2021-12-14 01:15:59 +0300

    media

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 'use strict';
 
-const form = document.querySelector('.send-form'),
-      btn = form.querySelector('.send-btn'),
-      input = form.querySelector('input'),
-      out = document.querySelector('.output');
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.send-form'),
+          btn = form.querySelector('.send-btn'),
+          input = form.querySelector('input'),
+          out = document.querySelector('.output');
 
 
-btn.addEventListener('click', () => {
-    console.log(input.value);
-    out.textContent = toLower(input.value);
+    btn.addEventListener('click', () => {
+        console.log(input.value);
+        out.textContent = toLower(input.value);
+    });
+
+    function toLower (text){
+        return text.toLowerCase();
+    }
 });
-
-function toLower (text){
-    return text.toLowerCase();
-}
-
